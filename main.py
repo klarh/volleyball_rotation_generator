a427a92339a9bd80594f9434757f747b4c01e689
Matthew Spellings <matthew.p.spellings@gmail.com>
2024-12-23 23:43:30 -0500

    Bump

--- main.py
+++ main.py
@@ -8,18 +8,8 @@ import numpy as np
 import app_lib
 
 # setter, middle, outside
-positions = np.array([
-    (1., .1, .2), # matthew
-    (.1, 0, 1), # enrique
-    (0, .3, 1), # zack
-    (0, 0, 1), # elvis
-    (0, 1, 1e-2), # sam
-    (0, 1, 1e-2), # johnny
-    (1, 0, 1e-2), # ivan
-    (1, 0, .5), # byron
-])
-
-names = np.array(['matthew', 'enrique', 'zack', 'elvis', 'sam', 'johnny', 'ivan', 'byron'])
+positions = np.random.random((7, 3))
+names = np.array(['Player {}'.format(chr(ord('a') + i)) for i in range(len(positions))])
 
 t = app_lib.ui.PlayerTable()
 for (name, pos) in zip(names, positions):
